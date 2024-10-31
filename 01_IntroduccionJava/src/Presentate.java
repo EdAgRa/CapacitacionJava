8451a2ce16fb4c92d8469a0630227d099ba4fd7e
EdAgRa <eduard.aguacia@gmail.com>
2024-10-31 11:49:15 -0500

    Renombre para organizar la cronologia

---
+++ 01_IntroduccionJava/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Eduard Aguacía Ramirez");
+        System.out.println("Edad: 45 Años");
+        System.out.println("Pais: Colombia");
+    }
+}
